4ac66d35c20794395203ec5e4be9ae9fffd2470a
moritzguehring <moritz.guehring@gmail.com>
2020-12-19 18:31:04 +0100

    styling

--- src/views/Task_1.vue
+++ src/views/Task_1.vue
@@ -1,39 +1,56 @@
 <template>
   <div class="task">
     <card :desc="desc" index="1"></card>
-    <input v-model="awnser" type="text" placeholder="Antwort"/>
-    <div @click="checkAwnser"> Überprüfen </div>
+    <input v-model="awnser" type="text" placeholder="Antwort" />
+    <div class="check" @click="checkAwnser">Überprüfen</div>
     <div class="response">
       <div v-show="response === true">true</div>
       <div v-show="response === false">false</div>
-
     </div>
   </div>
 </template>
 
 <script>
-
-import card from '@/components/Card.vue';
+import card from "@/components/Card.vue";
 // @ is an alias to /src
 
 export default {
-  data(){
-    return{
-      desc: "erste Aufgabe",
+  data() {
+    return {
+      desc: "Was war deine erste Fernreise?",
       awnser: "",
-      response: undefined
-    }
+      response: undefined,
+    };
   },
   methods: {
     checkAwnser() {
-      console.log(this.awnser)
-
-    }
-
+      console.log(this.awnser);
+    },
   },
   name: "Task_1",
   components: {
-    card
+    card,
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.check {
+  display: flex;
+  width: 200px;
+  height: 60px;
+
+  /* greyRed */
+
+  background: #000;
+  color: #fff;
+  border-radius: 10px;
+
+  font-size: 24px;
+  font-weight: 700;
+  align-items: center;
+  justify-content: center;
+
+  margin: 67px auto 0;
+}
+</style>

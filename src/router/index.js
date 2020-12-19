77354447262798942ecea36bd06d8a1803db1906
moritzguehring <moritz.guehring@gmail.com>
2020-12-19 18:49:28 +0100

    ads firework on success

--- src/router/index.js
+++ src/router/index.js
@@ -10,14 +10,14 @@ const routes = [
     name: 'task_1',
     component: task_1
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  {
+    path: '/success',
+    name: 'Success',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Success.vue')
+  }
 ]
 
 const router = new VueRouter({
